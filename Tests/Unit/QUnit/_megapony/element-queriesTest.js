734aa886f6cc51c15602da9db25cd576cc783ffd
gpaul <g.paul@opendo.at>
2013-11-07 10:28:34 +0100

    [TASK] Adds unit tests for failing selector text normalizing

--- Tests/Unit/QUnit/_megapony/element-queriesTest.js
+++ Tests/Unit/QUnit/_megapony/element-queriesTest.js
@@ -7,6 +7,9 @@ test('CSS-normalizer returns correct rules', function () {
 	equal(fixture.normalize('.l-facts header.megapony-max-width-220 + ul'), '.l-facts header.megapony-max-width-220 + ul');
 	equal(fixture.normalize('.megapony-rwd-table-container.megapony-max-width-400 .rwd-table'), '.megapony-rwd-table-container.megapony-max-width-400 .rwd-table');
 	equal(fixture.normalize('.megapony-max-width-800#max-width-target'), '#max-width-target.megapony-max-width-800');
+	equal(fixture.normalize('.carousel .megapony-max-width-1000#page'), '.carousel #page.megapony-max-width-1000');
+	equal(fixture.normalize('.megapony-max-width-480#extended-header .megapony-delimiter .facts'), '#extended-header.megapony-max-width-480 .megapony-delimiter .facts');
+	equal(fixture.normalize('.news-list :nth-child(1).megapony-object-column.megapony-max-width-410 + .megapony-object-column'), '.news-list .megapony-object-column.megapony-max-width-410:nth-child(1) + .megapony-object-column');
 });
 
 test('selectorContainsElementQuery() returns correct boolean', function () {
