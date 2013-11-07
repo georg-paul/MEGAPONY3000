5c08a4ede5164d1b65362089d06537e23c677602
pbergsmann <p.bergsmann@opendo.at>
2013-11-07 20:44:46 +0100

    [TASK] Adds unit-tests for calculation functions

--- Tests/Unit/QUnit/_megapony/element-queriesTest.js
+++ Tests/Unit/QUnit/_megapony/element-queriesTest.js
@@ -15,3 +15,27 @@ test('getTargetSelector returns the correct target', function() {
 	equal(fixture.getTargetSelector('.megapony-rwd-table-container.megapony-max-width-400 .rwd-table'), '.megapony-rwd-table-container');
 	equal(fixture.getTargetSelector('.megapony-max-width-480#extended-header .megapony-delimiter .facts'), '#extended-header');
 });
+
+test('getMaxWidth returns the max-width', function() {
+	var subject = new ElementQueries();
+	equal(subject.getMaxWidth('.news-list .megapony-object-column.megapony-max-width-410:nth-child(1) + .megapony-object-column'), 410);
+	equal(subject.getMaxWidth('.news-list .megapony-object-column.megapony-min-width-410:nth-child(1) + .megapony-object-column'), false);
+});
+
+test('getMinWidth returns the min-width', function() {
+	var subject = new ElementQueries();
+	equal(subject.getMinWidth('.news-list .megapony-object-column.megapony-max-width-410:nth-child(1) + .megapony-object-column'), false);
+	equal(subject.getMinWidth('.news-list .megapony-object-column.megapony-min-width-410:nth-child(1) + .megapony-object-column'), 410);
+});
+
+test('getMaxHeight returns the max-height', function() {
+	var subject = new ElementQueries();
+	equal(subject.getMaxHeight('.news-list .megapony-object-column.megapony-max-height-410:nth-child(1) + .megapony-object-column'), 410);
+	equal(subject.getMaxHeight('.news-list .megapony-object-column.megapony-min-height-410:nth-child(1) + .megapony-object-column'), false);
+});
+
+test('getMinHeight returns the min-height', function() {
+	var subject = new ElementQueries();
+	equal(subject.getMinHeight('.news-list .megapony-object-column.megapony-max-height-410:nth-child(1) + .megapony-object-column'), false);
+	equal(subject.getMinHeight('.news-list .megapony-object-column.megapony-min-height-410:nth-child(1) + .megapony-object-column'), 410);
+});
